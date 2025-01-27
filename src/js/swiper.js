bef83eca45aa3ea0160be03c10462ab3d492cbad
Eduard <sekaneduard@gmail.com>
2025-01-27 23:30:20 +0200

    add new page

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,5 +1,7 @@
 let swiper1 = null;
 let swiper2 = null;
+let swiper3 = null;
+let swiper4 = null;
 
 function toggleSwiper() {
   const isMobile = window.innerWidth < 1024; // Условие для мобильных устройств и планшетов
@@ -46,6 +48,48 @@ function toggleSwiper() {
         },
       });
     }
+
+    // Инициализация третьего слайдера
+    if (!swiper3) {
+      swiper3 = new Swiper('#slider3', {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        loop: true,
+        pagination: {
+          el: '#slider3 .swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '#slider3 .swiper-button-next',
+          prevEl: '#slider3 .swiper-button-prev',
+        },
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+      });
+    }
+
+    // Инициализация четвертого слайдера
+    if (!swiper4) {
+      swiper4 = new Swiper('#slider4', {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        loop: true,
+        pagination: {
+          el: '#slider4 .swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '#slider4 .swiper-button-next',
+          prevEl: '#slider4 .swiper-button-prev',
+        },
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+      });
+    }
   } else {
     // Уничтожение первого слайдера
     if (swiper1) {
@@ -57,6 +101,18 @@ function toggleSwiper() {
     if (swiper2) {
       swiper2.destroy(true, true);
       swiper2 = null;
+    }
+
+    // Уничтожение третьего слайдера
+    if (swiper3) {
+      swiper3.destroy(true, true);
+      swiper3 = null;
+    }
+
+    // Уничтожение четвертого слайдера
+    if (swiper4) {
+      swiper4.destroy(true, true);
+      swiper4 = null;
     }
   }
 }
